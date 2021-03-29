292650f847f99241dd2a5ea27b27379d6fbde4b4
c0dezer019 <c0dezer019@zohomail.com>
2021-03-29 10:37:32 -0500

    Modified route methods.

--- main/bot_routing.py
+++ main/bot_routing.py
@@ -11,19 +11,19 @@ def user_index():
     if request.method == 'GET':
         return get_all_users()
     elif request.method == 'POST':
-        return add_user(**request.form)
+        return add_user(data = request.get_json())
     else:
         raise Exception('That method isn\'t allowed here.')
 
 
-@bot.route('/users/<int:user_id>', methods = ['GET', 'PUT', 'DELETE'])
+@bot.route('/users/<int:user_id>', methods = ['GET', 'PATCH', 'DELETE'])
 def manage_user(user_id):
     if request.method == 'GET':
         return get_user(user_id)
-    elif request.method == 'PUT':
-        return update_user(user_id, **request.form)
+    elif request.method == 'PATCH':
+        return update_user(user_id, **request.get_json())
     elif request.method == 'DELETE':
-        return remove_user(user_id)
+        return remove_user(user_id, **request.get_json())
     else:
         raise Exception('That method isn\'t allowed here.')
 
@@ -33,7 +33,7 @@ def server_index():
     if request.method == 'GET':
         return get_all_servers()
     elif request.method == 'POST':
-        return add_server(**request.form)
+        return add_server(**request.get_json())
     else:
         raise Exception('That method isn\'t allowed here.')
 
@@ -43,7 +43,7 @@ def manage_server(server_id):
     if request.method == 'GET':
         return get_server(server_id)
     elif request.method == 'PUT':
-        return update_server(server_id, **request.form)
+        return update_server(server_id, **request.get_json())
     elif request.method == 'DELETE':
         return remove_server(server_id)
     else:
