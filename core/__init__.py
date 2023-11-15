329011ac043ab520c7829594bea00bac9d68c41d
Brian Blankenship <c0dezer019@zohomail.com>
2023-11-15 01:09:02 -0600

    Refactor for readability

--- core/__init__.py
+++ core/__init__.py
@@ -6,24 +6,19 @@ from dotenv import load_dotenv
 from flask import Flask
 from pathlib import Path
 
-env_path = Path('', '../.env')
-load_dotenv(dotenv_path = env_path)
+
+env_path = Path("", "../.env")
+load_dotenv(dotenv_path=env_path)
 
 
-def create_app():
-    flask_app = Flask(__name__, instance_relative_config = True)
+def create_app(config_class="DevConfig"):
+    flask_app = Flask(__name__, instance_relative_config=True)
 
-    if not os.getenv('MODE'):
-        flask_app.config.from_object('core.config.ProductionConfig')
-    elif os.getenv('MODE') == 'testing':
-        flask_app.config.from_object('core.config.TestConfig')
-    elif os.getenv('MODE') == 'development':
-        flask_app.config.from_object('core.config.DevConfig')
+    flask_app.config.from_object(f"core.config.{config_class}")
 
-    from core.config import db
+    from core.config import sql
 
-    db.init_app(flask_app)
-
+    sql.init_app(flask_app)
 
     try:
         os.makedirs(flask_app.instance_path)
@@ -31,6 +26,7 @@ def create_app():
         pass
 
     from core.graphql.routing import bot
+
     flask_app.register_blueprint(bot)
 
     return flask_app
