fc0bdb972c5f7532569a1c8deb6bd820c7859355
Brian Blankenship <c0dezer019@zohomail.com>
2023-11-19 10:56:10 -0600

    Added programmetic entry for ASGI.

--- asgi.py
+++ asgi.py
@@ -1,3 +1,4 @@
+import uvicorn
 from asgiref.wsgi import WsgiToAsgi
 
 from core import create_app
@@ -5,3 +6,6 @@ from core import create_app
 flask_app = create_app()
 
 asgi_app = WsgiToAsgi(flask_app)
+
+if __name__ == "__main__":
+    uvicorn.run(asgi_app)
