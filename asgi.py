232971c205e99cbd0c16d82822823bcc1210531b
Brian Blankenship <c0dezer019@zohomail.com>
2023-11-19 16:36:01 -0600

    Creating a factory for the asgi server.

--- asgi.py
+++ asgi.py
@@ -3,9 +3,8 @@ from asgiref.wsgi import WsgiToAsgi
 
 from core import create_app
 
-flask_app = create_app()
+def create_asgi():
+    flask_app = create_app()
+    asgi_app = WsgiToAsgi(flask_app)
 
-asgi_app = WsgiToAsgi(flask_app)
-
-if __name__ == "__main__":
-    uvicorn.run(asgi_app)
+    return asgi_app
