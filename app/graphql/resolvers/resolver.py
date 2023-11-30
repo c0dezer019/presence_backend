7fc3fc25002e1486a2fe3184710f0b3414d009b0
Brian Blankenship <c0dezer019@zohomail.com>
2023-11-30 13:03:39 -0600

    Error handling

--- app/graphql/resolvers/resolver.py
+++ app/graphql/resolvers/resolver.py
@@ -4,6 +4,8 @@ from typing import Optional
 # Third party modules
 from strawberry import ID
 from sqlalchemy import delete, select
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm.exc import UnmappedInstanceError
 
 # Internal modules
 from app.database import session
@@ -19,7 +21,6 @@ class Resolver:
             except (NameError, AttributeError):
                 pass
 
-
     def __init__(self):
         self.db = session
 
@@ -27,14 +28,20 @@ class Resolver:
         guild: Guild = Guild(**kwargs)
 
         self.db.add(guild)
-        self.db.commit()
+
+        try:
+            self.db.commit()
+        except IntegrityError:
+            print("Already exists.")
 
         return guild
 
     def updater(
         self, guild_id: ID, member_id: Optional[ID], **kwargs
     ) -> Guild | MemberShard:
-        guild: Guild = self.db.execute(select(Guild).filter_by(guild_id=guild_id)).scalar_one()
+        guild: Guild = self.db.execute(
+            select(Guild).filter_by(guild_id=guild_id)
+        ).scalar_one()
         member: MemberShard | None = (
             self.db.execute(
                 select(MemberShard).filter_by(guild_id=guild_id)
@@ -52,21 +59,27 @@ class Resolver:
 
             return member
 
+    def delete_entity(
+        self, guild_id: ID, member_id: Optional[ID]
+    ) -> Guild | MemberShard | None:
+        entity: MemberShard | Guild | None = (
+            self.db.query(Guild if not member_id else MemberShard)
+            .filter_by(guild_id=guild_id)
+            .first()
+        )
 
-    def delete_entity(self, guild_id: ID, member_id: Optional[ID]) -> Guild | MemberShard:
-        entity: MemberShard | Guild = self.db.get(
-            MemberShard, {"guild_id": guild_id, "member_id": member_id}
-        ) if member_id else self.db.get(Guild, {"guild_id": guild_id})
-
-        self.db.delete(entity)
-        self.db.commit()
+        try:
+            self.db.delete(entity)
+            self.db.commit()
+        except UnmappedInstanceError:
+            pass
 
         return entity
 
     def prune(self, guild_id: ID):
         member_shard_table = MemberShard.__table__
 
-        stmt = delete(member_shard_table).where(
+        stmt = delete(MemberShard).where(
             member_shard_table.c.guild_id == guild_id
         )
         self.db.execute(stmt)
