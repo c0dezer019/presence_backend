f20f8a4ae16c864c78db82c1fa01678db82e6f4d
Brian Blankenship <c0dezer019@zohomail.com>
2023-12-12 10:09:59 -0600

    test: Refactored into class tests and added edge cases.
    - Tests every function of the resolver.

--- tests/database_tests/test_db.py
+++ tests/database_tests/test_db.py
@@ -1,74 +1,21 @@
+"""
+Test to make sure database is properly setup.
+"""
+
 import pytest
 
-from datetime import datetime
-from fastapi.testclient import TestClient
-
-from app.database.database import SessionLocal, test_engine
-from app.database.models import Base, Guild, MemberShard
-from main import app
-
-Base.metadata.create_all(bind=test_engine)
+from app.database import database
 
 
-@pytest.fixture(scope="session", autouse=True)
-def db():
-    try:
-        db = SessionLocal()
-        yield db
-    finally:
-        db.close()
-        Base.metadata.drop_all(bind=test_engine)
+class TestDB:
+    @pytest.fixture
+    def db(self):
+        session = database
 
+        yield session
 
-def test_table_creation(app):
-    with app.app_context():
-        assert sql.engine.dialect.has_table(sql.engine.connect(), "members")
-        assert sql.engine.dialect.has_table(sql.engine.connect(), "guilds")
+        session.session.close()
 
-
-def test_create(app):
-    guild1 = Guild(
-        name="Taco Truck",
-        guild_id=123456,
-        last_activity="Texting",
-        last_active_ts=datetime(2020, 12, 5, 11, 30, 5),
-        last_active_channel="5674913213",
-        date_added=datetime(2020, 4, 3, 2, 45),
-    )  # type: ignore
-    guild3 = Guild(
-        name="Baristas Unite", guild_id=12344556, date_added=datetime(2021, 1, 1, 12, 0)
-    )  # type: ignore
-    user1 = Member(
-        member_id=1,
-        username="TestMember",
-        discriminator=1234356789,
-        date_added=datetime(2021, 3, 23, 5, 45, 12),
-    )  # type: ignore
-    user2 = Member(
-        member_id=2,
-        username="TestMember2",
-        discriminator=12345,
-        last_activity="Texting",
-        last_active_server="1234567890",
-        last_active_channel="5674913213",
-        last_active_ts=datetime(2020, 12, 5, 11, 30, 5),
-        date_added=datetime(2020, 4, 3, 2, 45),
-    )  # type: ignore
-    user3 = Member(
-        member_id=3,
-        username="TestMember3",
-        discriminator=158239784,
-        date_added=datetime(2021, 1, 1, 12, 0),
-    )  # type: ignore
-
-    guild1.members.append(user1)
-    guild1.members.append(user2)
-    guild3.members.append(user2)
-
-    with app.app_context():
-        sql.session.add(guild1)
-        sql.session.add(guild3)
-        sql.session.add(user1)
-        sql.session.add(user2)
-        sql.session.add(user3)
-        sql.session.commit()
+    def test_table_creation(self, db):
+        assert db.engine.dialect.has_table(db.engine.connect(), "member_shards")
+        assert db.engine.dialect.has_table(db.engine.connect(), "guilds")
---
+++ tests/database_tests/test_graphql_resolvers.py
@@ -0,0 +1,105 @@
+from typing import List
+
+import pytest
+from fastapi import HTTPException
+from sqlalchemy.exc import IntegrityError, NoResultFound
+
+from app.database import engine
+from app.database.models import Base, Guild, MemberShard
+from app.graphql.resolvers import Resolver
+
+Base.metadata.drop_all(engine)
+Base.metadata.create_all(engine)
+
+
+class TestResolvers:
+    @pytest.fixture(scope="class")
+    def resolver(self):
+        return Resolver()
+
+    @pytest.fixture(scope="class")
+    def guild(self, resolver):
+        guild_id = 1234
+        name = "Test Guild"
+        instance = resolver.guild(guild_id, name)
+
+        return instance
+
+    @pytest.fixture(scope="class")
+    def member(self, resolver):
+        guild_id = 1234
+        guild_name = "Test Guild"
+        member_id = 456
+        username = "Test User"
+        discriminator = 1234
+
+        member = resolver.member(guild_id, guild_name, member_id, username, discriminator)
+
+        return member
+
+    def test_create_guild_success(self, resolver, guild):
+        guild_id = 1234
+        name = "Test Guild"
+
+        assert isinstance(guild, Guild)
+        assert guild.guild_id == guild_id
+        assert guild.name == name
+        assert guild in resolver.db.session
+
+    def test_create_member_success(self, resolver, member, guild):
+        member_id = 456
+        username = "Test User"
+        discriminator = 1234
+
+        assert isinstance(member, MemberShard)
+        assert member.member_id == member_id
+        assert member.username == username
+        assert member.discriminator == discriminator
+        assert member.guild_id == guild.guild_id
+        assert member in resolver.db.session
+
+    def test_update_existing_guild_with_valid_guild_id_and_kwargs(self, resolver):
+        updated_guild = resolver.update_guild(1234, name="Updated Guild Name")
+
+        assert isinstance(updated_guild, Guild)
+        assert updated_guild.guild_id == 1234
+        assert updated_guild.name == "Updated Guild Name"
+
+    def test_create_new_guild_with_existing_guild_id_raises_http_exception(self, resolver):
+
+        with pytest.raises(HTTPException) as exc_info:
+            resolver.guild(1234, "Test Guild2")
+
+            assert exc_info.value.status_code == 400
+            assert exc_info.value.detail == "You cannot create another guild with a duplicate ID."
+
+    def test_create_a_member_shard_with_a_duplicate_discriminator(self, resolver):
+        with pytest.raises(HTTPException) as exc_info:
+            resolver.member(1234, "Test Guild", 789, "Test User", 1234)
+
+            assert exc_info.value.status_code == 400
+            assert exc_info.value.detail == "You cannot create another member with a duplicate discriminator."
+
+    def test_update_non_existing_guild(self, resolver):
+        with pytest.raises(NoResultFound):
+            resolver.update_guild(1235, name="Updated Guild Name")
+
+    def test_update_non_existing_member(self, resolver):
+        with pytest.raises(NoResultFound):
+            resolver.update_member_shard(12345, 1234, nickname="Joe")
+
+    def test_delete_guild(self, guild, resolver):
+        deleted_guild = resolver.delete_guild(guild.guild_id)
+
+        assert isinstance(deleted_guild, Guild)
+
+    def test_delete_member(self, guild, member, resolver):
+        deleted_member = resolver.delete_member_shard(guild.guild_id, member.member_id)
+
+        assert isinstance(deleted_member, MemberShard)
+
+    def test_prune_members(self, guild, member, resolver):
+        pruned_members = resolver.prune(guild.guild_id)
+
+        assert len(pruned_members) > 0
+        assert member in pruned_members
